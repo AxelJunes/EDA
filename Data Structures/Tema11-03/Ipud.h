6d742ec986f56fa045585f4acd96a737d6fa071c
AxelJunes <axel.junestrand@hotmail.com>
2017-06-01 13:59:01 +0200

    Functions implemented

--- Data Structures/Tema11-03/Ipud.h
+++ Data Structures/Tema11-03/Ipud.h
@@ -6,12 +6,14 @@
 //Type declarations
 
 //Song id
+typedef string Artista;
 typedef string IdSong;
+typedef int Duracion;
 
 // Song information
 typedef struct {
-	string artista;
-	int duracion;
+	Artista a;
+	Duracion d;
 }InfoSong;
 
 /**
@@ -29,14 +31,18 @@ class Ipud {
 		* Default constructor
 		*/
 		Ipud() {
-
+			this->songs = *new HashMap<IdSong, InfoSong>();
+			this->playlist = *new List<IdSong>();
+			this->played = *new List<IdSong>();
 		}
 
 		/**
 		* Parameter constructor
 		*/
-		Ipud() {
-
+		Ipud(HashMap<IdSong, InfoSong> songs, List<IdSong> playlist, List<IdSong> played) {
+			this->songs = songs;
+			this->playlist = playlist;
+			this->played = played;
 		}
 
 		// Getters and setters
@@ -72,42 +78,76 @@ class Ipud {
 		* Añade la canción S del artista A con duración D al Ipud.
 		* Si ya existe una canción con el mismo nombre, la operación dará error.
 		*/
-		void addSong() {
-
+		void addSong(IdSong s, Artista a, Duracion d) { // O(1)
+			if (this->songs.contains(s))
+				throw invalid_argument("Ya existe una canción con ese nombre!");
+			else{
+				// Si no hay ningún error, insertamos la canción en el HashMap
+				InfoSong info;
+				info.a = a;
+				info.d = d;
+				this->songs.insert(s, info);
+			}
 		}
 
 		/**
 		* Añade la canción S al final de la lista de reproducción. Si la canción ya se encontraba en la lista,
 		* entonces no se añade. Si la canción no está en el Ipud se devuelve error.
 		*/
-		void addToPlaylist() {
-
+		void addToPlaylist(IdSong s) { // O(played.length())
+			if (!this->songs.contains(s))
+				throw invalid_argument("La canción no existe en el iPud!");
+			// Si la canción existe en el iPud
+			bool encontrada = false;
+			List<IdSong> &played = this->played;
+			// Iteramos sobre la playlist para buscar la canción
+			List<IdSong>::ConstIterator it = played.cbegin();
+			while (!encontrada && it != played.cend()){
+				if (it.elem() == s)
+					encontrada = true;
+				it++;
+			}
+			if (!encontrada)
+				this->playlist.push_back(s);
 		}
 
 		/**
 		* Devuelve la primera canción de la lista de reproducción. Si la lista de reproducción es vacía,
 		* devuelve un error.
 		*/
-		InfoSong current() {
-			InfoSong c;
-
-			return c;
+		IdSong current() { // O(1)
+			if (this->playlist.empty())
+				throw invalid_argument("No hay canciones en la playlist!");
+			return this->playlist.front();
 		}
 
 		/**
 		* La primera canción de la lista de reproducción abandona la lista de reproducción y se registra como
 		* reproducida. Si la lista es vacía, la acción no tiene efecto.
 		*/
-		void play() {
-
-		}
+		//void play() {
+		//	if (this->playlist.empty())
+		//		return;
+		//	// Si la lista de reproducción no está vacía
+		//	this->
+		//}
 
 		/**
 		* Devuelve la suma de las duraciones de las canciones que integran la lista de reproducción actual.
 		* Si es vacía, se devuelve 0.
 		*/
-		int totalTime() {
-			return 0;
+		int totalTime() { // O(playlist.length())
+			int time = 0;
+			List<IdSong> &playlist = this->playlist;
+			// Iteramos sobre la playlist para sumar las duraciones
+			List<IdSong>::ConstIterator it = playlist.cbegin();
+			while (it != playlist.cend()){
+				//Buscamos la duración de la canción en el HashMap
+				int duracion = this->songs.find(it.elem()).value.d;
+				time += duracion;
+				it++;
+			}
+			return time;
 		}
 
 		/**
@@ -115,11 +155,28 @@ class Ipud {
 		* Si el número de canciones reproducidas es menor que N, se devolverán todas. La lista no tiene repeticiones,
 		* de manera que si una canción se ha reproducido más de una vez, sólo figurará la reproducción más reciente.
 		*/
-		List<IdSong> recent() {
-
+		List<IdSong> recent(int N) { // O(N)
+			if (this->played.size() < N)
+				return this->played;
+			List<IdSong> &played = this->playlist;
+			List<IdSong> recent;
+			// Iteramos sobre la lista de reproducidas
+			List<IdSong>::ConstIterator it = played.cbegin();
+			int n = 0;
+			while (n < N && it != played.cend()){
+				//Metemos en la lista de recientes las canciones por las que pase el iterador
+				recent.push_back(it.elem());
+				it++;
+				//Incrementamos el contador de canciones leídas
+				n++;
+			}
+			return recent;
 		}
 
-		void deleteSong() {
-
+		/**
+		* Elimina todo rastro de la canción S del iPud. Si la canción no existe, la operación no tiene efecto.
+		*/
+		void deleteSong(IdSong s) {
+			this->songs.erase(s);
 		}
 };
--- Data Structures/Tema11-03/main.cpp
+++ Data Structures/Tema11-03/main.cpp
@@ -1,10 +1,79 @@
 // Author: Axel Junestrand
 
+#include <iomanip>
 #include <iostream>
+#include <stdexcept>
+#include <string>
+#include <fstream>
 using namespace std;
 
+#include "Ipud.h"
+
+
+#ifndef DOMJUDGE
+
+ifstream in("cola.txt");
+
+auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+
+#endif
+
+bool resuelve() {
+
+	string inst;
+	cin >> inst;
+	if (!cin) return false;
+
+	Ipud ipud;
+	IdSong song;
+	InfoSong songInfo;
+
+	for (; inst != "FIN";) {
+		try {
+			if (inst == "addSong"){
+				cin >> song;
+				cin >> songInfo.artista;
+				cin >> songInfo.duracion;
+				ipud.addSong(song, songInfo.artista, songInfo.duracion);
+			}
+			if (inst == "addToPlaylist"){
+				cin >> song;
+				ipud.addToPlaylist(song);
+			}
+			if (inst == "current"){
+				IdSong result = ipud.current();
+				cout << "Current " << result << endl;
+			}
+			if (inst == "play"){
+				ipud.play();
+			}
+			if (inst == "totalTime"){
+				cout << "Tiempo total " << ipud.totalTime() << endl;
+			}
+			if (inst == "recent"){
+				int recent;
+				cin >> recent;
+				cout << "Las " << recent << " mas recientes" << endl;
+				auto vec = ipud.recent(recent);
+				for (auto p : vec) {
+					cout << "\t " << p << endl;
+				}
+				cout << "----" << endl;
+			}
+			if (inst == "deleteSong"){
+				cin >> song;
+				ipud.deleteSong(song);
+			}
+		}
+		catch (invalid_argument e) {
+			cout << e.what() << '\n' << "----\n";
+		}
+		cin >> inst;
+	}
+	return true;
+}
+
 int main() {
-
-
+	while (resuelve()){};
 	return 0;
 }
